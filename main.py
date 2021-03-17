00060059d533d236f10cd8ad424876607264d042
matriculus <pradeeshvishnu@outlook.com>
2021-03-18 00:31:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,48 @@
 import requests
 from urllib import parse
 import os
-
-filename = "BingWallpaper"
-i = 0
-while os.path.exists(filename + f"{i}.jpg"):
-    i += 1
+import tkinter as tk
 
 
-url = "https://www.bing.com"
-response = requests.get(url)
+def getBingWallpaper():
+    filename = os.path.join("BingWallpaper")
+    i = 0
+    while os.path.exists(filename + f"{i}.jpg"):
+        i += 1
 
-for div in response.text.split("style="):
-    if ".jpg" in div:
-        text = div.split(".jpg")[0]
-        if "url" in text:
-            wallpaper_url = parse.urljoin(url, text.split("url(")[-1]) + ".jpg"
-            image = requests.get(wallpaper_url)
-            filename1 = filename + f"{i}.jpg"
-            i += 1
-            with open(filename1, 'wb') as f:
-                f.write(image.content)
+    url = "https://www.bing.com"
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
+
+    response = requests.get(url, headers=headers)
+    temp = []
+    filename1 = ""
+    for div in response.text.split("style="):
+        if ".jpg" in div:
+            text = div.split(".jpg")[0]
+            if "url" in text:
+                wallpaper_url = parse.urljoin(url, text.split("url(")[-1]) + ".jpg"
+                if wallpaper_url in temp:
+                    continue
+                else:
+                    temp.append(wallpaper_url)
+                image = requests.get(wallpaper_url)
+                filename1 = filename + f"{i}.jpg"
+                i += 1
+                with open(filename1, 'wb') as f:
+                    f.write(image.content)
+                
+    
+    return filename1
+
+if __name__ == "__main__":
+    backgroundFile = getBingWallpaper()
+
+class Application(tk.Tk):
+    geo = "192x108"
+    background = backgroundFile
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Bing Wallpaper!")
+        self.config(bg=self.background)
+        self.geometry(self.geo)
+        self.pack()
